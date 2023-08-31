a49394e4b1325fd656daf5a47fa0083c6e7f12a2
Miguel Novaes <miguel.novaesx@gmail.com>
2023-08-31 17:05:56 -0300

    menu responsivo mobile

---
+++ 1_REACT_ROUTER_DOM/4_navegacao/src/pages/Usuarios.jsx
@@ -0,0 +1,12 @@
+import {Link} from "react-router-dom"
+
+function Usuarios() {
+  return (
+    <div>
+      <h1>Usuarios</h1>
+      <Link to={"/"}>voltar</Link>
+    </div>
+  )
+}
+
+export default Usuarios
